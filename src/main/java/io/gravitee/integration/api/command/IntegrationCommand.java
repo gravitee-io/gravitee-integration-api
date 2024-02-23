6a6e5ed5d13bb11ecebae5eb897ee8b16437de99
Remi Baptiste <remi.baptiste@graviteesource.com>
2024-02-23 11:29:50 +0100

    feat: init integration api with commands, model and plugin interface

---
+++ src/main/java/io/gravitee/integration/api/command/IntegrationCommand.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.integration.api.command;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.gravitee.exchange.api.command.Command;
+import io.gravitee.exchange.api.command.Payload;
+
+/**
+ * @author Guillaume LAMIRAND (guillaume.lamirand at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public abstract class IntegrationCommand<P extends Payload> extends Command<P> {
+
+    protected IntegrationCommand(final IntegrationCommandType type) {
+        super(type.name());
+    }
+}
